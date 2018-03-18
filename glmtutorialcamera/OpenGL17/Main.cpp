dc13ea6244801b7343bd94048683e523942e8d8c
buttburger <butt_burger@outlook.com>
2018-03-18 14:34:56 +0800

    Update Main.cpp

--- glmtutorialcamera/OpenGL17/Main.cpp
+++ glmtutorialcamera/OpenGL17/Main.cpp
@@ -80,38 +80,6 @@ void display()
 	glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
 	glLoadIdentity();
 
-	/*
-	glm::vec3 position = glm::vec3( 0, 0, 5 ); 
-	// Initial horizontal angle : toward -Z
-	float horizontalAngle = 3.14f;
-	// Initial vertical angle : none
-	float verticalAngle = 0.0f;
-	// Initial Field of View
-	float initialFoV = 45.0f;
-
-	float speed = 3.0f; // 3 units / second
-	float mouseSpeed = 0.005f;
-	double xpos=0, ypos=0;
-
-	//glfwGetCursorPos(window, &xpos, &ypos);
-	// Reset mouse position for next frame
-	//glfwSetCursorPos(window, 1024/2, 768/2);
-	// Compute new orientation
-	horizontalAngle += float(SCREEN_WIDTH/2 - xpos );
-	verticalAngle   += float(SCREEN_HEIGHT/2 - ypos );
-	// Direction : Spherical coordinates to Cartesian coordinates conversion
-	glm::vec3 direction(
-		cos(verticalAngle) * sin(horizontalAngle), 
-		sin(verticalAngle),
-		cos(verticalAngle) * cos(horizontalAngle)
-	);
-	// Right vector
-	glm::vec3 right = glm::vec3(sin(horizontalAngle - 3.14f/2.0f), 
-		0, cos(horizontalAngle - 3.14f/2.0f));
-	// Up vector
-	glm::vec3 up = glm::cross( right, direction );
-	*/
-
 	GLuint uniformModel = glGetUniformLocation(prog_hdlr, "model");
 	GLuint uniformView = glGetUniformLocation(prog_hdlr, "view");
 	GLuint uniformProjection = glGetUniformLocation(prog_hdlr, "projection");
